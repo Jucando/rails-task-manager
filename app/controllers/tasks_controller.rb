9cc286d45a90a5a2331aad5f7d531b315b3d67f3
Julien Chim <julien.chim@gmail.com>
2018-05-15 15:23:33 +0200

    ok

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,46 @@
 class TasksController < ApplicationController
+  before_action :init_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    init_task
+  end
+
+  def new
+    @task = Task.new # So that we can use the keys to create the form
+  end
+
+  def create
+    @task = Task.create(task_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def edit
+    init_task
+  end
+
+  def update
+    init_task
+    @task.update(task_params)
+    redirect_to task_path(@task.id)
+  end
+
+  def destroy
+    init_task
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed) # Get the task hash, and select only keep only the parameters you want to use (so no admin = true can be done)
+  end
+
+  def init_task
+    @task = Task.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  # As a user, I can list tasks
+
+  resources :tasks
+
+  # root to: "tasks#index"
+
+  # # As a user, I can list tasks
+  # get "/tasks", to: "tasks#index", as: "tasks"
+
+  # # As a user, I can add a new task
+  # get "/tasks/new", to: "tasks#new", as: "new_task"
+  # post "/tasks", to: "tasks#create"
+
+  # # As a user, I can view the details of a task
+  # get "/tasks/:id", to: "tasks#show", as: "task"
+
+  # # As a user, I can edit a task
+  # get "/tasks/:id/edit", to: "tasks#edit", as: "edit_task"
+  # patch "/tasks/:id", to: "tasks#update"
+
+  # delete "/tasks/:id", to: "tasks#destroy"
 end
